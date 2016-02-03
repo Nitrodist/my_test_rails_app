8d421f7bf51112260cd2209a53c391f0590063d1
Mark Campbell <nitrodist@gmail.com>
2016-02-03 10:55:47 +0000

    Test should fail and it does

--- test/integration/my_test.rb
+++ test/integration/my_test.rb
@@ -1,6 +1,9 @@
+require 'test_helper'
+
 class MyTest < ActionController::IntegrationTest
 
   test "an integration level test knows about something being raised in the controller" do
     get "/"
+    assert_response 500
   end
 end
